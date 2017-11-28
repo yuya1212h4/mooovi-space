12b5b6718d57c3afa89367c5b1ddaae6e0156982
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-11-29 08:30:42 +0900

    add users resources on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
   devise_for :users
   root to: "posts#index"
+  resources :posts
+  resources :users, only: [:index, :show] do
+    collection do
+      get 'search'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
