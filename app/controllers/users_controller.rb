c1501cefa4df09d6cd9dc09503a334fab383021d
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-11-29 19:14:56 +0900

    fix show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
+
   def index
     @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def search
     params[:search] ||= []
     @user = User.where("nickname LIKE(?)", "%#{params[:search]}%")
   end
+
 end
