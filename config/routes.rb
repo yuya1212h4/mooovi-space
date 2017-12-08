82b4e44eefdb1cd281072e166b7235e2a3ab0093
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-12-08 18:25:52 +0900

    add devise routes on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
   root to: "posts#index"
   resources :posts
   resources :users, only: [:index, :show, :edit, :update] do
