0839602d199849c3177a7c9d286a016e254869c9
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-12-17 16:48:06 +0900

    create maps links

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   end
   resources :admins, only: [:index, :show, :edit, :update]
   resources :relationships, only: [:create, :destroy]
+  resources :maps, only: [:index]
 end
