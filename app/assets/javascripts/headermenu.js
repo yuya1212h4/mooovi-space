fbbfe71585cd982cf0e909cd855f78f62ea86c1f
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-12-23 11:43:09 +0900

    add turbolinks:load on headmenu.js

--- app/assets/javascripts/headermenu.js
+++ app/assets/javascripts/headermenu.js
@@ -1,5 +1,7 @@
-$(function() {
-    $('#navToggle').click(function(){//headerに .openNav を付加・削除
-        $('header').toggleClass('openNav');// 指定した要素にクラス名を付けたり、消したりすることが出来る
-    });
+$(document).on('turbolinks:load', function() {
+  $(function() {
+      $('#navToggle').click(function(){//headerに .openNav を付加・削除
+          $('header').toggleClass('openNav');// 指定した要素にクラス名を付けたり、消したりすることが出来る
+      });
+  });
 });
