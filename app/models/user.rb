e0326db30549a2014b24a7f35d42d728d9106b8d
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-12-23 15:25:32 +0900

    add association on models files

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :addresses
+  accepts_nested_attributes_for :addresses
 
   geocoded_by :address
   after_validation :geocode
